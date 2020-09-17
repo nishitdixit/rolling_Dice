703c97b4b109bcba6ffa5dd985e4eaf24b9ea438
Nishit <nishitdxt@gmail.com>
2020-09-18 00:08:01 +0530

    'Added MyApp'

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
-import 'dart:math';
 import 'package:rolling_Dice/splash.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: SplashScreen(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(MyApp(),
+    
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: SplashScreen(),
+    );
+  }
 }
